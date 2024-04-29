d0b48435db025c6783a0231598bbaf4eb998f415
Markyrodri2000 <markus.9.mr@gmail.com>
2024-04-29 15:02:19 +0200

    SSH MV

--- API-Modelos/api.py
+++ API-Modelos/api.py
@@ -1,31 +1,59 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-#from chat_config import config
-from together import responder
+import paramiko
 
 app = Flask(__name__)
 CORS(app)
-#modelo = config()
+
+HOSTNAME = '52.143.134.115'
+USERNAME = 'azureuser'
+PORT = 50000
+def COMMAND_POST(mensaje):
+    return f"""curl -X POST \
+-H "Content-Type: application/json" \
+-d '{{"mensaje":"{mensaje}"}}' \
+http://localhost:8000"""
+
+KEY = "./id_rsa"
+
+class SSH:
+    def __init__(self):
+
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(HOSTNAME, username=USERNAME, port=PORT, key_filename=KEY)
+        print("Conexión SSH establecida correctamente...")
+
+    def enviar_mensaje(self, mensaje):
+        stdin, stdout1, stderr = self.ssh.exec_command(COMMAND_POST(mensaje))
+        respuesta = stdout1.read().decode("utf-8")
+        if stdout1.channel.recv_exit_status() == 0:
+            return respuesta
+        else:
+            print("Respuesta no generada correctamente")
+            return "Error"
+
+    def terminar_conexion(self):
+        print("Conexión SSH cerrada correctamente...")
+        self.ssh.close()
+
+ssh = SSH()
 
 @app.route('/', methods=['POST'])
 def post_data():
-    datos = request.json
-    if 'nombre' in datos and datos['nombre'] == "Marc":
-        data = "Hola Marc"
-    else:
-        """question = datos['mensaje']
-        response = modelo.run(question)
-        print(response)
-        print(datos['mensaje'])"""
-        response = responder(datos['mensaje'])
-        data = response
-    
-    return jsonify({"mensaje": data})
+    mensaje = request.json['mensaje']
+    respuesta = ssh.enviar_mensaje(mensaje)
+    print(respuesta)
+    return jsonify({"mensaje": respuesta})
 
 @app.route('/', methods=['GET'])
 def get_data():
     data = {'message': 'Hola desde el servidor Flask!'}
     return jsonify(data)
+
+def shutdown_session(exception=None):
+    ssh.terminar_conexion()
+    print("La aplicación Flask se ha cerrado.")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
